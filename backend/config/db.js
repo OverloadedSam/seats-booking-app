90e3e4f7474084d1188e0481f25cea70c2b49a0d
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-20 11:05:21 +0530

    Function for database connection

---
+++ backend/config/db.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+/**
+ * @DESC  Establish database connection.
+ */
+const connectDB = async () => {
+  const URI = process.env.DB_CONNECTION_STRING;
+  const options = {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    dbName: process.env.DB_NAME,
+  };
+
+  try {
+    mongoose.set('strictQuery', true);
+    await mongoose.connect(URI, options);
+    console.log('SUCCESSFULLY CONNECTED TO DB!');
+  } catch (error) {
+    console.log('CONNECTION FAILED TO DB!');
+    console.log(error);
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
