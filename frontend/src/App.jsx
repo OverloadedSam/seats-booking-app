ca03b5edd39175eff1fec052de6de0ad37ff473a
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-20 19:58:06 +0530

    Define theme for the frontend

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,20 @@
+import { useMemo } from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import createTheme from '@mui/material/styles/createTheme';
+import ThemeProvider from '@mui/material/styles/ThemeProvider';
+import CssBaseline from '@mui/material/CssBaseline';
+import { themeOptions } from './theme';
+
 const App = () => {
+  const theme = useMemo(() => createTheme(themeOptions), [themeOptions]);
+
   return (
     <div className='app'>
-      <h1>Welcome to train seats booking app</h1>
+      <BrowserRouter>
+        <ThemeProvider theme={theme}>
+          <CssBaseline />
+        </ThemeProvider>
+      </BrowserRouter>
     </div>
   );
 };
