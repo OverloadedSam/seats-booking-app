d6310ab3cf5071ee52bc3958ab61c39a4ba9bcf9
Samdeesh <samdeeshsingh001@gmail.com>
2023-01-21 23:40:11 +0530

    Add inline docs for clarity

--- backend/controllers/trains.js
+++ backend/controllers/trains.js
@@ -1,15 +1,25 @@
 const Train = require('../models/train');
 const asyncHandler = require('../middlewares/asyncHandler');
 
+/**
+ * @DESC     Creates a train in the DB.
+ * @ROUTE    POST /api/train
+ * @ACCESS   Public
+ * @PARAMS   {string} name.body.required - Name for the train.
+ * @PARAMS   {number} number.body.required - Number of the train.
+ * @PARAMS   {string} startStation.body.optional - Starting station of the train.
+ * @PARAMS   {string} terminationStation.body.optional - Terminating station of the train.
+ * @RETURNS  {object} 201 - {success, status, ...train}
+ */
 module.exports.createTrain = asyncHandler(async (req, res, next) => {
   const trainPayload = req.body;
 
   const train = new Train(trainPayload);
   await train.save();
 
-  return res.json({
+  return res.status(201).json({
     success: true,
-    status: 200,
+    status: 201,
     ...train._doc,
   });
 });
--- frontend/src/utils/seatFunctions.js
+++ frontend/src/utils/seatFunctions.js
@@ -1,9 +1,22 @@
+/**
+ * @DESC     Calculates the seat number.
+ * @PARAMS   {number} rowIndex - Row index of a seat matrix (2D array).
+ * @PARAMS   {number} columnIndex - Column index of a seat matrix (2D array).
+ * @PARAMS   {number} numberOfColumns - Number of columns in a seat matrix (2D array).
+ * @RETURNS  {number} - Represents the seat number (non-zero based indexing).
+ */
 export const getSeatNumber = (rowIndex, columnIndex, numberOfColumns) => {
   let position = rowIndex * numberOfColumns + columnIndex;
 
   return position + 1;
 };
 
+/**
+ * @DESC     Get the array of reserved seats for a user.
+ * @PARAMS   {Array<Array<seatObject>>} seats - A seat matrix represented by a 2D array.
+ * @PARAMS   {string} userName - Logged in user.
+ * @RETURNS  {Array<number>} - Represents the seats numbers reserved by the given user.
+ */
 export const getUserReservedSeats = (seats, userName) => {
   const userSeats = [];
   const numberOfColumns = seats[0].length;
